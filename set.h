fbcf38b11b528bd3d8dcbd523a65dcb55b0862e2
jaw653 <jawax10@gmail.com>
2017-11-24 12:55:01 -0600

    initial commit

---
+++ set.h
@@ -0,0 +1,20 @@
+/* This is the header file describing the disjoint set
+ * data structure.
+ * Author: Dr. John Lusth, University of Alabama
+ */
+
+#ifndef __SET_INCLUDED__
+#define __SET_INCLUDED__
+
+#include <stdio.h>
+
+typedef struct set SET;
+
+extern SET *newSET(void (*d)(FILE *,void *));
+extern int makeSET(SET *d,void *value);
+extern int findSET(SET *d,int index);
+extern int unionSET(SET *d,int index1,int index2);
+extern int countSET(SET *d);
+extern int displaySET(FILE *,SET *d);
+
+#endif
