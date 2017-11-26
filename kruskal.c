aafffcb836c23b0eeca59ca12202336fa90c1590
jaw653 <jawax10@gmail.com>
2017-11-26 16:30:36 -0600

    kruskal updates to fix seg fault

--- kruskal.c
+++ kruskal.c
@@ -40,7 +40,7 @@ static DA *kruskal(DA *);
 int main(int argc, char *argv[]) {
   if (argc == 1)
     return 0;
-    
+
   FILE *graphFile = fopen(argv[1], "r");
 
   DA *arr = newDA(displayINTEGER);
