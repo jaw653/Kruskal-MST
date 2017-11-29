f0ae3040c9120587a74e3ae6d8dd224c9d24d6fe
jaw653 <jawax10@gmail.com>
2017-11-28 22:08:22 -0600

    fixed union set and find set. find now uses indexes instead of values (duh, you dunce), and now unioning to that index rather than another node

--- set.c
+++ set.c
@@ -25,6 +25,8 @@ struct NODE {
   struct NODE *parent;
 };
 
+static NODE *newNODE(void *, NODE *, int);
+
 SET *newSET(void (*d)(FILE *, void *)) {
   SET *set = malloc(sizeof(struct set));
   set->list = newDA(d);
@@ -35,10 +37,11 @@ SET *newSET(void (*d)(FILE *, void *)) {
 }
 
 int makeSET(SET *d, void *value) {
-  insertDA(d->list, value);
+  NODE *valNode = newNODE(value, NULL, sizeDA(d->list));
+  insertDA(d->list, valNode);
   d->numReps += 1;
 
-  return sizeDA(d->list);
+  return (sizeDA(d->list) - 1);
 }
 
 int findSET(SET *d, int index) {
@@ -49,7 +52,7 @@ int findSET(SET *d, int index) {
   if (getINTEGER(foundNode->value) == getINTEGER(parent->value))
     return index;
 
-  return findSET(d, getINTEGER(parent->value));
+  return findSET(d, parent->index);
 }
 
 int unionSET(SET *d, int index1, int index2) {
@@ -60,9 +63,9 @@ int unionSET(SET *d, int index1, int index2) {
     return 0;
 
   NODE *node1 = getDA(d->list, index1);
-  NODE *node2 = getDA(d->list, index2);
+//  NODE *node2 = getDA(d->list, index2);
 
-  node1->parent = node2;
+  node1->parent = getDA(d->list, rep2);
 
   d->numReps -= 1;
   return 1;
@@ -72,6 +75,23 @@ int countSET(SET *d) {
   return d->numReps;
 }
 
+/*
 int displaySET(FILE *fp, SET *d) {
-  
+
+}
+*/
+
+/******************************************************************************/
+/***                         Private Functions                              ***/
+/******************************************************************************/
+static NODE *newNODE(void *value, NODE *parent, int index) {
+  NODE *node = malloc(sizeof(struct NODE));
+  node->value = value;
+  node->index = index;
+  if (parent == NULL)
+    node->parent = node;
+  else
+    node->parent = parent;
+
+  return node;
 }
